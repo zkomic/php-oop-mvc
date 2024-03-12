b8a38bebb98fe274a267674f55303ba5da022b19
Zrinka <60172572+zkomic@users.noreply.github.com>
2024-03-12 12:27:45 +0100

    database_and_includes

--- project/app/config/config.php
+++ project/app/config/config.php
@@ -1,5 +1,11 @@
 <?php 
 
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'tmvc');
+
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 
