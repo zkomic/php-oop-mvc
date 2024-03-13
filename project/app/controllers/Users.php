7da78cbb186c4341f5899dcf1f67f6981f2f5dee
Zrinka <60172572+zkomic@users.noreply.github.com>
2024-03-13 09:05:42 +0100

    login

--- project/app/controllers/Users.php
+++ project/app/controllers/Users.php
@@ -120,10 +120,24 @@ class Users extends Controller
                 $data['password_error'] = 'Please enter password';
             }
 
+            // Check for user/email
+            if ($this->userModel->findUserByEmail($data['email'])) {
+                // User found
+            } else {
+                $data['email_error'] = 'No user registered with that email address';
+            }
+
             // Make sure errors are empty
             if (empty($data['email_error']) && empty($data['password_error'])) {
-                //Validates
-                die('SUCCESS');
+                // Check and set login user
+                $loggedInUser = $this->userModel->login($data['email'], $data['password']);
+                if ($loggedInUser) {
+                    // Create session variables
+                    $this->createUserSession($loggedInUser);
+                } else {
+                    $data['password_error'] = 'Password incorrect.';
+                    $this->view('users/login', $data);
+                }
             } else {
                 // Load view with errors
                 $this->view('users/login', $data);
@@ -140,6 +154,34 @@ class Users extends Controller
 
             // Load view
             $this->view('users/login', $data);
+        }
+    }
+
+    public function createUserSession($user)
+    {
+        $_SESSION['user_id'] = $user->id;
+        $_SESSION['user_email'] = $user->email;
+        $_SESSION['user_name'] = $user->name;
+
+        redirect('pages/index');
+    }
+
+    public function logout()
+    {
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_email']);
+        unset($_SESSION['user_name']);
+        session_destroy();
+
+        redirect('users/login');
+    }
+
+    public function isLoggedIn()
+    {
+        if (isset($_SESSION['user_id'])) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
