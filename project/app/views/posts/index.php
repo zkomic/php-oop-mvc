c7cad88e1a9b5c595866d82cc70689d961d4cc26
Zrinka <60172572+zkomic@users.noreply.github.com>
2024-03-20 11:19:41 +0100

    posts

---
+++ project/app/views/posts/index.php
@@ -0,0 +1,14 @@
+<?php require APPROOT . '/views/inc/header.php' ?>
+<br>
+<div class="row">
+    <div class="col-md-6">
+        <h1>Posts</h1>
+    </div>
+    <div class="col-md-6">
+        <a href="<?php echo APPROOT; ?>/posts/add" class="btn btn-primary float-right">
+            <i class="fa fa-pencil"></i> Add post
+        </a>
+    </div>
+</div>
+
+<?php require APPROOT . '/views/inc/footer.php' ?>
